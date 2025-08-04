0cb071df6b7c4916bd257e327c4f601fb2026d86
Adhil Shan A P <66194129+adhilshan@users.noreply.github.com>
2025-08-04 12:38:48 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,8 +3,10 @@ import numpy as np
 import time
 import psutil
 from numba import njit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 # Optimized distance sum using Numba
 @njit
 def pairwise_distance_sum(coords):
